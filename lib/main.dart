2041026ab7da4a32ed9c7e8702c6891c264d4dad
ArpitTripathi <arpitnathtripathi@gmail.com>
2020-05-26 11:10:44 +0530

    final app

---
+++ lib/main.dart
@@ -0,0 +1,256 @@
+import 'package:covid_19/constant.dart';
+import 'package:covid_19/widgets/counter.dart';
+import 'package:covid_19/widgets/my_header.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Covid 19',
+      theme: ThemeData(
+          scaffoldBackgroundColor: kBackgroundColor,
+          fontFamily: "Poppins",
+          textTheme: TextTheme(
+            bodyText2: TextStyle(color: kBodyTextColor),
+          )),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final controller = ScrollController();
+  double offset = 0;
+
+  int infected = 20;
+  int deaths = 5;
+  int recovered = 10;
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(onScroll);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  void onScroll() {
+    setState(() {
+      offset = (controller.hasClients) ? controller.offset : 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        controller: controller,
+        child: Column(
+          children: <Widget>[
+            MyHeader(
+              image: "assets/icons/Drcorona.svg",
+              textTop: "All you need",
+              textBottom: "is stay at home.",
+              offset: offset,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              height: 60,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(25),
+                border: Border.all(
+                  color: Color(0xFFE5E5E5),
+                ),
+              ),
+              child: Row(
+                children: <Widget>[
+                  SvgPicture.asset("assets/icons/maps-and-flags.svg"),
+                  SizedBox(width: 20),
+                  Expanded(
+                    child: DropdownButton(
+                      isExpanded: true,
+                      underline: SizedBox(),
+                      icon: SvgPicture.asset("assets/icons/dropdown.svg"),
+                      value: "Computer Science & Engineering",
+                      items: [
+                        'Computer Science & Engineering',
+                        'Information Technology',
+                        'Electronics & Communication',
+                        'Mechanical Engineering',
+                        'Civil Engineering'
+                      ].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: "Case Update\n",
+                              style: kTitleTextstyle,
+                            ),
+
+                          ],
+                        ),
+                      ),
+                      Spacer(),
+
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              offset: Offset(0, 4),
+                              blurRadius: 30,
+                              color: kShadowColor,
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Counter(
+                                  color: kInfectedColor,
+                                  number: infected,
+                                  title: "Infected",
+                                ),
+                                Counter(
+                                  color: kDeathColor,
+                                  number: deaths,
+                                  title: "Deaths",
+                                ),
+                                Counter(
+                                  color: kRecovercolor,
+                                  number: recovered,
+                                  title: "Recovered",
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                FloatingActionButton(
+                                  heroTag: "btn1",
+                                  onPressed: () {
+                                    setState(() {
+                                      infected+=1;
+                                    });
+                                  },
+                                  child: Icon(Icons.add),
+                                  backgroundColor: kInfectedColor,
+                                  tooltip: 'Infected',
+                                ),
+                                FloatingActionButton(
+                                  heroTag: "btn2",
+                                  onPressed: () {
+                                    setState(() {
+                                      deaths+=1;
+                                    });
+                                  },
+                                  child: Icon(Icons.add),
+                                  backgroundColor: kDeathColor,
+                                  tooltip: 'Deaths',
+                                ),
+                                FloatingActionButton(
+                                  heroTag: "btn3",
+                                  onPressed: () {
+                                    setState(() {
+                                      recovered+=1;
+                                    });
+                                  },
+                                  child: Icon(Icons.add),
+                                  backgroundColor: kRecovercolor,
+                                  tooltip: 'Recovered',
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Spread of Virus",
+                        style: kTitleTextstyle,
+                      ),
+                      Text(
+                        "See details",
+                        style: TextStyle(
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    padding: EdgeInsets.all(20),
+                    height: 178,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(0, 10),
+                          blurRadius: 30,
+                          color: kShadowColor,
+                        ),
+                      ],
+                    ),
+                    child: Image.asset(
+                      "assets/images/map.png",
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
